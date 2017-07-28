b09d0966830531c5a3c76449933239eaadfb83f1
Rajeev <rajeev@fastmail.com>
2017-07-28 12:26:40 -0400

    Initial Commit

---
+++ StaticTableViewResizing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StaticTableViewResizing
+//
+//  Created by Rajeev Ruparell on 2017-07-28.
+//  Copyright © 2017 Rajeev Ruparell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
